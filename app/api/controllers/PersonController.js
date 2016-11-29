8bebca234925151b21c971de62d0b4e9239a096b
Joseph Ramos <joseph.ramos@taller.net.br>
2016-11-29 03:50:44 -0500

    WIP

--- app/api/controllers/PersonController.js
+++ app/api/controllers/PersonController.js
@@ -6,6 +6,9 @@
  */
 
 module.exports = {
-	
-};
 
+	list: function(req,res) {
+		res.view();
+	},
+
+};
--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,103 +1,65 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	// $("#ok").hide();
 
-	$('#dni').blur(function(){
-		var dni  = $("#dni").val();
-		var dataString = 'dni='+dni;
-		$.ajax({
-	        type: "POST",
-	        url: "../models/validar.php",
-	        data: dataString,
-	        success: function(data) {
-	            $('#Info').fadeIn(1000).html(data);
-	        }
-	    });
+	$("#register-form").validate({
+		rules: {
+			dni: {
+				required: true,
+				minlength: 8,
+				maxlength: 8,
+			},
+			name_lastname: {
+				required: true,
+				minlength: 2
+			},
+			age: {
+				required: true,
+				minlength: 1
+			},
+			cellphone: {
+				required: true,
+				minlength: 9
+			},
+			institution: {
+				required: true,
+				minlength: 2
+			},
+			email: {
+				required:true,
+				email: true
+			},
+			//hora_ingreso: { minlength: 2, maxlength: 15},
+			reason: {
+				required: false
+			},
+		},
 
+		// messages: {
+		//     name: "Debe introducir su nombre.",
+		//     nom_ap: "Debe introducir su nombre y apellido.",
+		//     edad : "Debes introducir tu edad.",
+		//     celular : "Debe introducir un celular válido.",
+		//     institucion : "Debe ingresar su institucion.",
+		//     email : "El campo email es obligatorio.",
+		//     motivo : "El campo Mensaje es obligatorio.",
+		// },
 
+		submitHandler: function(form){
+			var arrayData = $(form).serializeArray()
+			var data = {};
+
+			arrayData.forEach(function (item) { data[item.name] = item.value })
+
+			$.ajax({
+				type: "POST",
+				url:"/api/v1/person",
+				data: data,
+				success: function(result){
+					$("#ok").html(data);
+					$("#ok").show();
+					$("#register-form").hide();
+				}
+			});
+		}
 	});
-	$("#register").click(function() {
-		//Obtenemos el valor del campo nombre
-		var dni  = $("#dni").val();
-		var nom_ap = $("#name_lastname").val();
-		var edad = $("#age").val();
-		var celular = $("#cellphone").val();
-		var institucion = $("#institution").val();
-		var email = $("#email").val();
-		var hora_ingreso = $("#check_in").val();
-		var motivo = $("#reason").val();
-
-		var dataString = {'dni' : dni,
-						 'name_lastname' : nom_ap,
-						 'age' : edad,
-						 'cellphone' : celular,
-						 'institution' : institucion,
-						 'email' : email,
-						 'check_in' : hora_ingreso,
-						 'reason' : motivo};
-		$.ajax({
-			type: "POST",
-			url: "/person",
-			data : dataString,
-			success: function() {
-		    	$('#register_form').html("<div id='message'></div>");
-		        $('#message').html("<h2>Tus datos han sido guardados correctamente!</h2>").hide().fadeIn(1500, function() {
-					$('#message').append("<a href='index.php?action=see'>Ver usuarios registrados</a>");
-		        });
-		    }
-		});
-		return false;
-	});
-
-
-});
-
-/*$(document).ready(function() {
-    $("#ok").hide();
-
-    $("#register-form").validate({
-        rules: {
-        	dni: {required: true, maxlength:8},
-            nom_ap: { required: true, minlength: 2},
-            edad: { required: true, minlength: 2},
-            celular: {required: true, minlength: 9},
-            institucion: { required: true, minlength: 2},
-            email: { required:true, email: true},
-            //hora_ingreso: { minlength: 2, maxlength: 15},
-            motivo: { required: false},
-        },
-        messages: {
-            name: "Debe introducir su nombre.",
-            nom_ap: "Debe introducir su nombre y apellido.",
-            edad : "Debes introducir tu edad.",
-            celular : "Debe introducir un celular válido.",
-            institucion : "Debe ingresar su institucion.",
-            email : "El campo email es obligatorio.",
-            motivo : "El campo Mensaje es obligatorio.",
-        },
-        submitHandler: function(form){
-            var dataString = {'dni' : dni,
-						 'nom_ap' : nom_ap,
-						 'edad' : edad,
-						 'celular' : celular,
-						 'institucion' : institucion,
-						 'email' : email,
-						 'hora_ingreso' : hora_ingreso,
-						 'motivo' : motivo};
-            $.ajax({
-                type: "POST",
-                url:"../models/registrar.php",
-                data: dataString,
-                success: function(data){
-                    $("#ok").html(data);
-                    $("#ok").show();
-                    $("#register-form").hide();
-                }
-            });
-        }
-    });
-});
-
-
-
-
-
-*/
+})
