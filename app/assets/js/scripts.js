23c1583bb58d32af97683068010a4638b2c24718
Joseph Ramos <joseph.ramos@taller.net.br>
2016-11-30 03:11:11 -0500

    Refactoring resgitration styles and scripts.

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
-	// $("#ok").hide();
 
-	$("#register-form").validate({
+	// Add an event listener to show message component.
+	document.addEventListener('newPerson', function(e) {
+		var $message = $('.message')
+		var $description = $message.find('.message__content___description')
+
+		$description.html(`<strong>${ e.detail.fullname }</strong> fue registrado exitosamente.`)
+		$message.addClass('message--show')
+
+		setTimeout( function(){
+			$message.removeClass('message--show')
+		}, 2000)
+	})
+
+
+	$('#register-form').validate({
 		rules: {
 			pid: {
 				required: true,
 				minlength: 8,
 				maxlength: 8,
 			},
-			name_lastname: {
+			fullname: {
 				required: true,
 				minlength: 2
 			},
@@ -28,38 +41,42 @@ $(document).ready(function() {
 				required:true,
 				email: true
 			},
-			//hora_ingreso: { minlength: 2, maxlength: 15},
 			reason: {
 				required: false
 			},
 		},
 
 		// messages: {
-		//     name: "Debe introducir su nombre.",
-		//     nom_ap: "Debe introducir su nombre y apellido.",
-		//     edad : "Debes introducir tu edad.",
-		//     celular : "Debe introducir un celular válido.",
-		//     institucion : "Debe ingresar su institucion.",
-		//     email : "El campo email es obligatorio.",
-		//     motivo : "El campo Mensaje es obligatorio.",
+		//     name: 'Debe introducir su nombre.',
+		//     nom_ap: 'Debe introducir su nombre y apellido.',
+		//     edad : 'Debes introducir tu edad.',
+		//     celular : 'Debe introducir un celular válido.',
+		//     institucion : 'Debe ingresar su institucion.',
+		//     email : 'El campo email es obligatorio.',
+		//     motivo : 'El campo Mensaje es obligatorio.',
 		// },
 
-		submitHandler: function(form){
+		submitHandler: function(form) {
 			var arrayData = $(form).serializeArray()
-			var data = {};
+			var data = {}
 
 			arrayData.forEach(function (item) { data[item.name] = item.value })
 
+			// Reset form.
+			form.reset()
+
 			$.ajax({
-				type: "POST",
-				url:"/api/v1/person",
+				type: 'POST',
+				url:'/api/v1/person',
 				data: data,
 				success: function(result){
-					$("#ok").html(data);
-					$("#ok").show();
-					$("#register-form").hide();
+					// Create the event
+					var event = new CustomEvent('newPerson', { detail: result })
+
+					// Dispatch/Trigger/Fire the event
+					document.dispatchEvent(event)
 				}
-			});
+			})
 		}
-	});
+	})
 })
